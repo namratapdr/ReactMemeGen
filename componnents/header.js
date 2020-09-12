31ea52bac342c4e78b4dd25a7e4a1067fd8f58c1
namratapdr <namratapodder138@gmail.com>
2020-09-12 16:54:04 +0530

    Adding the components for the meme gen

---
+++ componnents/header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export function Header(){
+   
+    return(
+        <div className="header">
+            <div className="logo"><img src="./logo.jpg" alt="logo"/></div>
+            <h1>Meme Generator</h1>
+        </div>
+    )
+    
+}
